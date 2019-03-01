3bfe94223dffe12c18d9bb02d8e04429c382be2c
Alex Makush <aliaksei.makuch@cogniteq.com>
2019-03-01 13:03:07 +0300

    fix second task

--- src/tasks/task_2.js
+++ src/tasks/task_2.js
@@ -2,6 +2,7 @@ export const task2 = () => {
   const task2 = document.querySelector('#task2')
   const input = document.querySelector('[data-js-task2]')
   const button = document.querySelector('[data-js-btn-2]')
+  const boxStyles = ``
   if (task2 == null) {
     return null
   }
@@ -12,8 +13,13 @@ export const task2 = () => {
     const COND3 = (inputValue > 0)
     if (COND1 && COND2 && COND3) {
       for (let i = 0; i < inputValue; i++) {
-        const block = document.querySelector('[data-js-box]').appendChild(document.createElement('div'))
-        block.style.cssText = `width: ${inputValue}px; height: ${inputValue}px; background: rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`
+        const block = document.querySelector('[data-js-box]')
+        const row = block.appendChild(document.createElement('div'))
+        row.style.cssText = 'width: 100%; margin-bottom: 15px; display: flex;'
+        for (let j = 0; j < inputValue; j++) {
+          const block = row.appendChild(document.createElement('div'))
+          block.style.cssText = `width: 200px; height: 50px; margin: 0 15px; background: rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`
+        }
       }
     } else {
       alert('Data error')
